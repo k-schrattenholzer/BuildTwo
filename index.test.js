e37f5b3e8196f01e21d93672bce526930798f133
‘Katie <ktschr11@gmail.com>
2021-11-05 10:10:32 -0700

    tests passing

--- index.test.js
+++ index.test.js
@@ -6,22 +6,26 @@ describe('getMethod', () => {
         const req = 
         `GET / HTTP/1.1
         Host: example.com
-        Accept-Language: us-en`
+        Accept-Language: us-en`;
+
+        const expectation = 'GET';
 
         const reqMethod = getMethod(req);
 
-        expect(reqMethod).toEqual('GET');
+        expect(reqMethod).toEqual(expectation);
     })
     it('when given an HTTP request, will return the method only', () => {
         
         const req = 
         `DELETE / HTTP/1.1
         Host: example.com
-        Accept-Language: us-en`
+        Accept-Language: us-en`;
+
+        const expectation = 'DELETE';
 
         const reqMethod = getMethod(req);
 
-        expect(reqMethod).toEqual('GET');
+        expect(reqMethod).toEqual(expectation);
     })
     
 })
