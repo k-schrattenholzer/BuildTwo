65e3f25260e9347ac1f12b1de37f049fb2c2a064
‘Katie <ktschr11@gmail.com>
2021-11-05 10:01:11 -0700

    tests written, not passing

--- index.test.js
+++ index.test.js
@@ -1,8 +1,27 @@
 const getMethod = require("./index.js");
 
 describe('getMethod', () => {
-    it('', () => {
+    it('when given an HTTP request, will return the method only', () => {
         
+        const req = 
+        `GET / HTTP/1.1
+        Host: example.com
+        Accept-Language: us-en`
+
+        const reqMethod = getMethod(req);
+
+        expect(reqMethod).toEqual('GET');
+    })
+    it('when given an HTTP request, will return the method only', () => {
+        
+        const req = 
+        `DELETE / HTTP/1.1
+        Host: example.com
+        Accept-Language: us-en`
+
+        const reqMethod = getMethod(req);
+
+        expect(reqMethod).toEqual('GET');
     })
     
 })
